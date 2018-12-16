f3394a2b9cbf06b459d71e801f5d90e69d63cf24
sam.d.atkins <sam.d.atkins@gmail.com>
2018-12-16 12:31:19 -0700

    Major refactor to seperate concerns

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import "@babel/polyfill";
-import { Document, Packer, Paragraph } from "docx";
 import * as fs from "fs";
+import { TabWriter } from "./tabWriter";
 import { getBestMatch, getTab } from "./ultimateGuitarSearcher";
 
-const maxLinesPerSong = 116;
-const linesInSinglePage = 67;
-const maxColumns = 90;
-
-// Have to use main because babel async won't work at top level
+// Have to use a wrapper function because babel won't recognize async at top level
 main();
 
 async function main() {
@@ -16,111 +12,30 @@ async function main() {
     .toString()
     .split("\r\n");
 
-  const doc = new Document(undefined, {
-    top: 500,
-    right: 500,
-    bottom: 500,
-    left: 500,
-  });
+  const tabWriter = new TabWriter();
 
-  doc.Styles.createParagraphStyle("monospaced")
-    .basedOn("Normal")
-    .font("Courier New");
-  doc.Styles.createParagraphStyle("tiny")
-    .basedOn("Normal")
-    .font("Courier New")
-    .size(12);
-
-  doc.addParagraph(new Paragraph("Table of Contents:").title());
-  songArray.map(song =>
-    doc.addParagraph(new Paragraph(song).style("monospaced")),
-  );
-  doc.addParagraph(new Paragraph().pageBreakBefore());
+  tabWriter.addTableOfContents(songArray);
 
   console.log(songArray);
 
   for (const song of songArray) {
-    await processSong(song, doc);
+    const tab = await getTabForSong(song, tabWriter);
+    tab && tabWriter.writeTabToDoc(tab);
   }
 
-  const packer = new Packer();
-
-  packer.toBuffer(doc).then(buffer => {
-    fs.writeFileSync("My Document.docx", buffer);
-  });
+  tabWriter.save("My Document.docx");
 }
 
-async function processSong(song, doc) {
-  console.log(song);
+async function getTabForSong(song) {
+  console.log(`Read in: ${song}`);
   const title = song.split(" - ")[0].replace(/\(.+\)/g, "");
   const artist = song.split(" - ").slice(-1);
-  console.log(`${title} - ${artist}`);
+  console.log(`Searching for: ${title} - ${artist}`);
   const match = await getBestMatch(artist, title);
   if (!match) {
     console.error(`Couldn't locate song: ${song}`);
     return;
   }
 
-  const tab = await getTab(match.url);
-  writeTabToDoc(tab, doc);
-}
-
-function writeTabToDoc(tab, doc) {
-  console.log(`writing ${tab.name} to doc`);
-  doc.addParagraph(
-    new Paragraph(`${tab.name} - ${tab.artist}`)
-      .style("monospaced")
-      .pageBreakBefore(),
-  );
-  doc.addParagraph(new Paragraph(`${tab.url}`).style("tiny"));
-  doc.addParagraph(new Paragraph("").style("monospaced"));
-
-  const tabLines = tab.content.text.split("\n");
-  const firstLineOfChords = tabLines.findIndex(line => line.includes("[ch]"));
-  const capoLine = tabLines.find(line => line.includes("capo"))
-    ? [tabLines.find(line => line.includes("capo"))]
-    : [];
-
-  if (hasAnyOverflowingLines(tab.content.text, capoLine, firstLineOfChords))
-    console.error(`${tab.name} contains lines that are too long\n`);
-
-  const textLines = capoLine.concat(
-    tab.content.text.split("\n").slice(firstLineOfChords, maxLinesPerSong),
-  );
-
-  textLines.map(line => writeLineToDoc(line, doc));
-
-  if (textLines.length < linesInSinglePage)
-    doc.addParagraph(new Paragraph().pageBreakBefore());
-}
-
-function writeLineToDoc(line, doc) {
-  const para = doc.createParagraph().style("monospaced");
-
-  const lineSplitByFormatting = line.split(/\[\/?ch\]/g);
-  
-  lineSplitByFormatting.forEach((lineFragment, index) => {
-    if (lineFragment.length === 0)
-      return;
-
-    if (index % 2 === 0)
-      para.createTextRun(lineFragment);
-    else
-      para.createTextRun(lineFragment).color("blue");
-  });
-}
-
-function hasAnyOverflowingLines(tabContent, capoLine, firstLineOfChords) {
-  const textLines = capoLine.concat(
-    tabContent
-      .replace(/\[ch\]/g, "")
-      .replace(/\[\/ch\]/g, "")
-      .split("\n")
-      .slice(firstLineOfChords, maxLinesPerSong),
-  );
-
-  if (textLines.filter(line => line.length > maxColumns).length > 0)
-    return true;
-
-  return false;
+  return await getTab(match.url);
 }
---
+++ src/tabWriter.js
@@ -0,0 +1,119 @@
+import { Document, Packer, Paragraph } from "docx";
+import * as fs from "fs";
+
+// todo: don't break page between chords/lyrics
+// todo: allow override of tab choice via URL
+
+const maxLinesPerSong = 116;
+const linesInSinglePage = 63;
+const maxColumns = 90;
+
+const _writeLineToDoc = Symbol("writeLineToDoc");
+const _hasAnyOverflowingLines = Symbol("hasAnyOverflowingLines");
+
+var doc;
+
+export class TabWriter {
+  constructor() {
+    doc = new Document(undefined, {
+      top: 500,
+      right: 500,
+      bottom: 500,
+      left: 500,
+    });
+
+    doc.Styles.createParagraphStyle("monospaced")
+      .basedOn("Normal")
+      .font("Courier New");
+    doc.Styles.createParagraphStyle("tiny")
+      .basedOn("Normal")
+      .font("Courier New")
+      .size(12);
+  }
+
+  save(fileName) {
+    const packer = new Packer();
+
+    packer.toBuffer(doc).then(buffer => {
+      fs.writeFileSync(fileName, buffer);
+    });
+  }
+
+  addTableOfContents(songArray) {
+    doc.addParagraph(new Paragraph("Table of Contents:").title());
+    songArray.map(song =>
+      doc.addParagraph(new Paragraph(song).style("monospaced")),
+    );
+    doc.addParagraph(new Paragraph().pageBreakBefore());
+  }
+
+  writeTabToDoc(tab) {
+    console.log(`Writing ${tab.name} - ${tab.artist} to doc`);
+    doc.addParagraph(
+      new Paragraph(`${tab.name} - ${tab.artist}`)
+        .style("monospaced")
+        .pageBreakBefore(),
+    );
+    doc.addParagraph(new Paragraph(`${tab.url}`).style("tiny"));
+    doc.addParagraph(new Paragraph("").style("monospaced"));
+
+    const tabLines = tab.content.text.split("\n");
+    const firstLineOfChords = tabLines.findIndex(line => line.includes("[ch]"));
+    const capoLine = tabLines.find(line => line.includes("capo"))
+      ? [tabLines.find(line => line.includes("capo"))]
+      : [];
+
+    if (
+      this[_hasAnyOverflowingLines](
+        tab.content.text,
+        capoLine,
+        firstLineOfChords,
+      )
+    )
+      console.error(`${tab.name} contains lines that are too long`);
+
+    const textLines = capoLine.concat(
+      tab.content.text.split("\n").slice(firstLineOfChords, maxLinesPerSong),
+    );
+
+    textLines.map((line, lineIndex) => this[_writeLineToDoc](line, lineIndex));
+
+    if (textLines.length < linesInSinglePage)
+      doc.addParagraph(new Paragraph().pageBreakBefore());
+  }
+
+  // private method
+  [_writeLineToDoc](line, lineIndex) {
+
+    const lineSplitByFormatting = line.split(/\[\/?ch\]/g);
+
+    // Don't split chord/lyric line pairs over a new page
+    if ((lineIndex + 1) == linesInSinglePage && lineSplitByFormatting.length > 1)
+      doc.addParagraph(new Paragraph(" "));
+
+    const para = doc.createParagraph().style("monospaced");
+
+    lineSplitByFormatting.forEach((lineFragment, index) => {
+      if (lineFragment.length === 0) return;
+
+      if (index % 2 === 0) para.createTextRun(lineFragment);
+      else para.createTextRun(lineFragment).color("blue");
+    });
+  }
+
+  // private method
+  [_hasAnyOverflowingLines](tabContent, capoLine, firstLineOfChords) {
+    const textLines = capoLine.concat(
+      tabContent
+        .replace(/\[ch\]/g, "")
+        .replace(/\[\/ch\]/g, "")
+        .split("\n")
+        .slice(firstLineOfChords, maxLinesPerSong),
+    );
+
+    if (textLines.filter(line => line.length > maxColumns).length > 0)
+      return true;
+
+    return false;
+  }
+}
