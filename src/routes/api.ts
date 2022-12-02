a3902d3d35c29c9157c218c4377f179c3c5fcd6d
jackconwayiv <52581258+jackconwayiv@users.noreply.github.com>
2022-12-01 18:28:30 -0700

    Added songbook "add song" endpoint. (#21)

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import songbookRouter from "./songbook";
 
 const apiRouter = Router();
 
 apiRouter.get("/test", (req, res) => res.send("hi"));
 
+apiRouter.use("/songbook", songbookRouter);
+
 export default apiRouter;
+
