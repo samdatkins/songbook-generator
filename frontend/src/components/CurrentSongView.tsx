aeed51e4a41abc456d89544bb8cc6668631052f9
atkins <sam@trufflesec.com>
2022-12-31 09:40:35 -0700

    Add json dump (#22)

--- frontend/src/components/CurrentSongView.tsx
+++ frontend/src/components/CurrentSongView.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { Song } from "../models/song";
 import {
   Box,
@@ -14,17 +13,11 @@ import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
 
 function CurrentSongView() {
   const [currentSong, setCurrentSong] = useState<Song>();
-  const splitTab =
-    currentSong &&
-    currentSong.tab
-      .replaceAll("[tab]", "")
-      .replaceAll("[/tab]", "")
-      .split("\n");
+  const splitTab = currentSong && currentSong.tab;
 
   useEffect(() => {
     async function getCurrentSong() {
-      const curSong = await axios.get<Song>("/live/sams-test/current");
-      setCurrentSong(curSong.data);
+      setCurrentSong({} as Song);
     }
 
     getCurrentSong();
@@ -32,41 +25,11 @@ function CurrentSongView() {
 
   return (
     <>
+      <span>hello</span>
       {!currentSong ? (
         <Spinner />
       ) : (
-        <Flex padding="1rem" flexDir="column">
-          <Flex flexDir="row" w="100%" justifyContent="space-between">
-            <Button colorScheme="blue">
-              <ArrowBackIcon />
-            </Button>
-            <Heading as="h2" display="inline-block" fontSize="2xl">
-              <Link href={currentSong.tabUrl}>
-                "{currentSong.title}" by {currentSong.artist}
-              </Link>{" "}
-              ({currentSong.current} of {currentSong.total})
-            </Heading>
-            <Button colorScheme="blue">
-              <ArrowForwardIcon />
-            </Button>
-          </Flex>
-          <Box mt="1rem" p="1rem" style={{ columnCount: 2, columnGap: "1rem" }}>
-            <pre style={{ fontSize: "1.25rem", fontFamily: "Ubuntu Mono" }}>
-              Tab:{" "}
-              {splitTab?.map((tabLine) => {
-                if (tabLine.includes("[ch]")) {
-                  return (
-                    <Text color="blue">
-                      {tabLine.replaceAll("[ch]", "").replaceAll("[/ch]", "")}
-                    </Text>
-                  );
-                } else {
-                  return <Text>{tabLine}</Text>;
-                }
-              })}
-            </pre>
-          </Box>
-        </Flex>
+        <Flex padding="1rem" flexDir="column"></Flex>
       )}
     </>
   );
--- src/routes/songbook.ts
+++ src/routes/songbook.ts
@@ -3,34 +3,38 @@ import {
   addSongToSession,
   isSongbookFull,
   isValidSongbookSession,
+  getAllSongbooksWithRelationships,
 } from "../db/repositories/songbook";
 import { getBestMatch, getTabForUrl } from "../tabSearcher";
+import { StatusCodes } from "http-status-codes";
 
 const alphaRegex = /[^0-9A-Za-z\ ]+/g;
 const songbookRouter = Router();
 
 songbookRouter.get("/:sessionKey/test", (req, res) =>
-  res.send(`hi ${req.params.sessionKey}`),
+  res.send(`hi ${req.params.sessionKey}`)
 );
+
+songbookRouter.get("/jsondump", async (req, res) => {
+  return res
+    .status(StatusCodes.OK)
+    .json(await getAllSongbooksWithRelationships());
+});
 
 songbookRouter.post("/:sessionKey/add", async (req, res) => {
   if (!(await isValidSongbookSession(req.params.sessionKey))) {
-    res.status(404);
-    return res.json("Invalid session key");
+    return res.status(StatusCodes.NOT_FOUND).json("Invalid session key.");
   }
 
   if (await isSongbookFull(req.params.sessionKey)) {
-    res.status(400);
-    return res.send(
-      `<p>Songbook full, no more requests allowed! </p><a href='/live/${req.params.sessionKey}/add><- Back</a>`,
-    );
+    return res
+      .status(StatusCodes.CONFLICT)
+      .json("Songbook full, no more requests allowed!");
   }
 
   const match = await getBestMatch(req.body.song.replace(alphaRegex, ""));
   if (!match) {
-    res.send(
-      `<p>No matches found :(</p><a href='/live/${req.params.sessionKey}/add><- Back</a>`,
-    );
+    res.status(StatusCodes.NOT_FOUND).json("Song match could not be found.");
   }
   try {
     const tab = (await getTabForUrl(match.url)) as any;
@@ -44,7 +48,7 @@ songbookRouter.post("/:sessionKey/add", async (req, res) => {
 
     addSongToSession(newSong, req.params.sessionKey);
 
-    return res.status(201).json("Song successfully added!");
+    return res.status(StatusCodes.CREATED).json("Song successfully added!");
   } catch (error) {
     return res.status(400).json("Couldn't find song. :(");
   }
